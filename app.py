085a58106714c6434b44a4b8a2e4577e7b88b9b4
Kim Cornwell <cornywell@gmail.com>
2022-11-29 20:49:24 +1300

    Functional create student and functional average of all students

--- app.py
+++ app.py
@@ -16,13 +16,31 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
+
+    sum_of_avg_grades = db.session.query(func.sum(Student.avg_grade).label("total_avg_grade")).all()[0][0] # strange indexing to unpack SA query
+    num_students = db.session.query(Student).count()
+    avg_all_students_avgs = sum_of_avg_grades / num_students
+
     student_result = None
     if request.args.get("id"):
         student_result = Student.query.get_or_404(request.args.get("id"))
     students_id_and_fullname = Student.query.with_entities(Student.id, Student.firstname, Student.lastname)
-    return render_template("index.html", fullnames=students_id_and_fullname, result=student_result)
+    return render_template("index.html", fullnames=students_id_and_fullname, result=student_result, avg_avgs=avg_all_students_avgs)
+
+
+@app.route('/create/', methods=['POST'])
+def create():
+    firstname = request.form['firstname']
+    lastname = request.form['lastname']
+    age = int(request.form['age'])
+    avg_grade = request.form['avg_grade']
+    student = Student(firstname=firstname, lastname=lastname, age=age, avg_grade=avg_grade)
+    db.session.add(student)
+    db.session.commit()
+
+    return redirect(url_for('index'))
 
 
 @app.post('/<id>/delete/')
@@ -31,6 +49,7 @@ def delete(id):
     db.session.delete(student)
     db.session.commit()
     return redirect(url_for('index'))
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')

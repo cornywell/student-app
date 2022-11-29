480a5e3f59c632cb4c36e633e26d04d9e3a689d5
Kim Cornwell <cornywell@gmail.com>
2022-11-30 07:58:05 +1300

    Correct label names

--- forms.py
+++ forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, IntegerField, DecimalField
 from wtforms.validators import DataRequired, NumberRange
 
 class StudentForm(FlaskForm):
-    firstname = StringField('firstname', validators=[DataRequired()])
-    lastname = StringField('lastname', validators=[DataRequired()])
-    age = IntegerField('age', validators=[DataRequired(), NumberRange(min=0, max=20)])
-    avg_grade = DecimalField('avg_grade', validators=[DataRequired(), NumberRange(min=0, max=100)])
+    firstname = StringField('First name', validators=[DataRequired()])
+    lastname = StringField('Last name', validators=[DataRequired()])
+    age = IntegerField('Age', validators=[DataRequired(), NumberRange(min=0, max=20)])
+    avg_grade = DecimalField('Average grade', validators=[DataRequired(), NumberRange(min=0, max=100)])

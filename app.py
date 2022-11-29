764129f65296c002e83d91145c314950dc95af48
Kim Cornwell <cornywell@gmail.com>
2022-11-29 18:41:21 +1300

    Delete functionality

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from werkzeug.exceptions import NotFound
@@ -17,13 +17,20 @@ db.init_app(app)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def Index():
+def index():
     student_result = None
     if request.args.get("id"):
         student_result = Student.query.get_or_404(request.args.get("id"))
-
     students_id_and_fullname = Student.query.with_entities(Student.id, Student.firstname, Student.lastname)
     return render_template("index.html", fullnames=students_id_and_fullname, result=student_result)
+
+
+@app.post('/<id>/delete/')
+def delete(id):
+    student = Student.query.get_or_404(id)
+    db.session.delete(student)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
